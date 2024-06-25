1670c7d7898062d54e1c5a0ea31c5b29fbc7a0bb
LCH-97 <leechanghoon12@gmail.com>
2024-06-25 15:59:51 +0900

    [Refactor] Product 리팩토링
    - BaseEntity 적용
    - BaseResponse 적용
    
    Close: #15, #16, #17, #18, #19, #20, #21, #22, #23, #24, #31, #32, #33, #34, #35, #36, #44

--- backend/src/main/java/com/woof/api/product/controller/AdminProductController.java
+++ backend/src/main/java/com/woof/api/product/controller/AdminProductController.java
@@ -1,9 +1,14 @@
 package com.woof.api.product.controller;
 
+import com.woof.api.common.BaseResponse;
 import com.woof.api.product.model.response.ProductManagerListRes;
+import com.woof.api.product.model.response.ProductManagerReadRes;
+import com.woof.api.product.model.response.ProductSchoolReadRes;
 import com.woof.api.product.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/product")
@@ -17,37 +22,39 @@ public class AdminProductController {
     }
 
     @RequestMapping(method = RequestMethod.PATCH, value = "/manager/check")
-    public ResponseEntity managerSchool(@RequestParam Long idx) {
-        productService.checkManager(idx);
-        return ResponseEntity.ok().body("상품 idx" + idx + "승인 완료");
+    public ResponseEntity<BaseResponse<Void>> managerSchool(@RequestParam Long idx) {
+        BaseResponse<Void> response = productService.checkManager(idx);
+        return ResponseEntity.ok().body(response);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/manager/list")
-    public ResponseEntity<ProductManagerListRes> adminListManager() {
-        return ResponseEntity.ok().body(productService.adminListManager());
+    public ResponseEntity<BaseResponse<List<ProductManagerReadRes>>> adminListManager() {
+        BaseResponse<List<ProductManagerReadRes>> response = productService.adminListManager();
+        return ResponseEntity.ok().body(response);
     }
 
     @RequestMapping(method = RequestMethod.PATCH, value = "/manager/delete")
-    public ResponseEntity<String> deleteManager(@RequestParam Long idx) {
-        productService.deleteManager(idx);
-        return ResponseEntity.ok().body("매니저 idx" + idx + "삭제 완료");
+    public ResponseEntity<BaseResponse<Void>> deleteManager(@RequestParam Long idx) {
+        BaseResponse<Void> response = productService.deleteManager(idx);
+        return ResponseEntity.ok().body(response);
     }
 
     @RequestMapping(method = RequestMethod.PATCH, value = "/school/check")
-    public ResponseEntity checkSchool(@RequestParam Long idx) {
-        productService.checkSchool(idx);
-        return ResponseEntity.ok().body("상품 idx" + idx + "승인 완료");
+    public ResponseEntity<BaseResponse<Void>> checkSchool(@RequestParam Long idx) {
+        BaseResponse<Void> response = productService.checkSchool(idx);
+        return ResponseEntity.ok().body(response);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/school/list")
-    public ResponseEntity adminListSchool() {
-        return ResponseEntity.ok().body(productService.adminListSchool());
+    public ResponseEntity<BaseResponse<List<ProductSchoolReadRes>>> adminListSchool() {
+        BaseResponse<List<ProductSchoolReadRes>> response = productService.adminListSchool();
+        return ResponseEntity.ok().body(response);
     }
 
     @RequestMapping(method = RequestMethod.PATCH, value = "/school/delete")
-    public ResponseEntity deleteSchool(@RequestParam Long idx) {
-        productService.deleteSchool(idx);
-        return ResponseEntity.ok().body("상품 idx" + idx + "삭제 완료");
+    public ResponseEntity<BaseResponse<Void>> deleteSchool(@RequestParam Long idx) {
+        BaseResponse<Void> response = productService.deleteSchool(idx);
+        return ResponseEntity.ok().body(response);
     }
 }
 
