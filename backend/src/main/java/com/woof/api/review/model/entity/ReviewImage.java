8775237b7d6c3d52aae306591b97858e04a5916b
jiieunn2 <93915072+jiieunn2@users.noreply.github.com>
2024-06-26 12:04:26 +0900

    [Refactor] Review 기본키 삭제

--- backend/src/main/java/com/woof/api/review/model/entity/ReviewImage.java
+++ backend/src/main/java/com/woof/api/review/model/entity/ReviewImage.java
@@ -14,9 +14,7 @@ import javax.persistence.*;
 @Setter
 @Builder
 public class ReviewImage extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idx;
+   
     private String filename; // S3에 저장된 파일명 (UUID 포함)
     private String originalFilename; // 원본 파일 이름을 저장할 필드 추가
 
