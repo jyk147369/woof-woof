fc8027f2bf8ab2b1e9846dec5dbc6c6ca0b04407
LCH-97 <leechanghoon12@gmail.com>
2024-07-10 17:49:55 +0900

    [Fix] Product 모든 기능 오류 Fix
    - 다 실행 됩니다.

--- backend/src/main/java/com/woof/api/product/model/response/ProductFileDto.java
+++ backend/src/main/java/com/woof/api/product/model/response/ProductFileDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductFileDto {
-    private Long id;
+    private Long idx;
     private String originalFilename;
     private String downloadUrl;
 }
