431ea239b60a766c658acfb320583b3500b05672
LCH-97 <leechanghoon12@gmail.com>
2024-07-15 10:03:09 +0900

    [Fix] ProductManagerCreateReq 수정
    - private String managerName; > member에서 가져오기 때문에 주석처리

--- backend/src/main/java/com/woof/api/product/model/request/ProductManagerCreateReq.java
+++ backend/src/main/java/com/woof/api/product/model/request/ProductManagerCreateReq.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Builder
 @Data
 public class ProductManagerCreateReq {
-    private String managerName;     // 매니저 이름
+//    private String managerName;     // 매니저 이름 > member에서 가져오기 때문에 주석처리
     private String gender;          // 매니저 성별
     private String businessNum;     // 매니저 번호
     private Integer price;          // 매니저 1달 이용 가격
